993fd125818d2f9b201b525e2b8b4ba1f0d3cb83
Amr Mohamed <punish3r297@gmail.com>
2025-10-16 18:50:11 +0300

    Add a Serializer for the products app.

---
+++ products/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username') # show user name
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'category', 'stock', 'image_url', 'owner', 'created_at']
