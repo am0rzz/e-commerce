511b619e7ccf80ebd5bedfbabdf9e74b37b1fb36
Amr Mohamed <punish3r297@gmail.com>
2025-10-16 20:07:11 +0300

    (feature): Implement search, filtering and paination for the products app.

--- products/views.py
+++ products/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Product
 from .serializers import ProductSerializer
 from.permessions import IsOwnerOrReadOnly
-
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 
 class ProductViewSet(viewsets.ModelViewSet):
   
     # a view with pre made list , create , retrieve , update , delete.
-    
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name', 'category']
+    filterset_fields = ['category']
+    pagination_class = StandardResultsSetPagination
 
     def perform_create(self, serializer):
 
